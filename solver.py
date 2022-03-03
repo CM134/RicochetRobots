09ae93651070a59cd44b263119eb13f0c81a862d
Christian Metz <cm@agrirobot.ai>
2022-03-03 10:19:55 +0100

    new structure with a lot of pseudo code. There is a lot of cleaning and coding to be done :-)

---
+++ solver.py
@@ -0,0 +1,12 @@
+# Pseudo code for implementation of the AI.
+
+class Solver
+def __init__(GAME):
+    def genericAlgo(self, GAME.board, agent_dict, goal):
+        return path
+    
+game = Ricochet(board_setup=1)
+
+Solver(game)
+
+for solver.path do move
---
+++ visualizer.py
@@ -0,0 +1,19 @@
+#Pseudo code
+
+class Visualizer:
+    def __init__(self,game):
+        self.game = game
+    
+    def draw_board(self):
+        for col and rows:
+            if nswe:
+                draw_line()
+        for agent:
+            draw agent in color at col row
+        
+        draw_goal()
+    
+    def show_moves:
+        game.availableMoves(agent)
+        draw_moves()
+    # in main show make & update game instance.
