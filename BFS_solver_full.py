6d4c02bff0e2213ac031bf95307946262e605976
rohboz <33161645+rohboz@users.noreply.github.com>
2022-03-21 15:02:01 +0100

    Changed BFS list to deque, and added backtracing for path in BFS

--- BFS_solver_full.py
+++ BFS_solver_full.py
@@ -3,7 +3,8 @@ from ricochet import Ricochet
 from visualizer import Visualizer
 import copy
 import pdb
-
+from collections import deque
+import time
 
 class Solver:
     def __init__(self, GAME):
@@ -14,11 +15,12 @@ class Solver:
         self.color = self.colorMap[self.goal['color']]
         rest_colors = list(self.colorMap.values())
         rest_colors.remove(self.color)
-        # Add other robots
+        # Get all 4 robots, where robot0 is the goal robot
         self.agent0 = getattr(self.Game, self.color)
         for i in range(3):
             setattr(self, ("agent"+str(i+1)),
                     getattr(self.Game, rest_colors[i]))
+        #Initialize BFS to perform search
         self.path = self.BFS()
 
     def getPoses(self):
@@ -33,22 +35,34 @@ class Solver:
             ag_i = getattr(self, "agent"+str(i_))
             ag_i["row"] = pos_[i_][0]
             ag_i["col"] = pos_[i_][1]
+    def backtracePath(self, pth, goalPos):
+        parent = 1
+        goalPath = []
+        parent_ = goalPos
+        #Backtrace path until depth 1
+        depth_ = 100
+        while depth_ > 1:
+            parent_, depth_, colorDir_ = pth[parent_]
+            goalPath.append(colorDir_)
+        return goalPath.reverse()
 
     def BFS(self):
         goal = ((self.goal["row"], self.goal["col"]))
+        #Initialize the relative path
         visited = []
-        queue = []
+        queue = deque()
         path = {}
 
         root_pos = self.getPoses()
         queue.append(root_pos)
         visited.append(root_pos)
-        path[root_pos] = []
+        path[root_pos] = (None, 0, (None, None)) # (Parent, Depth (Color, Direction))
+        #path[root_pos] = []
         depth = 0
         depth_old = depth
-        # print(root_pos)
-        while (depth < 10) and len(path) < 300000:
-            pos = queue.pop(0)
+        time0 = time.time()
+        while ((time.time()-time0) < 60) and (len(queue) != 0):
+            pos = queue.popleft()
             for i in range(4):  # Robot Level
                 self.setPoses(pos)  # Resets all poses to queue start positions
                 ag = getattr(self, "agent"+str(i))
@@ -63,15 +77,18 @@ class Solver:
                     if new_pos not in visited:
                         if (move_dir == goal) and i == 0:
                             print("FOUND GOAL!!!!")
-                            path[new_pos] = path[pos].copy()
-                            path[new_pos].append(
-                                (self.colorMap[ag['name']], dir))
-                            return path[new_pos]
+                            #path[new_pos] = path[pos].copy()
+                            path[new_pos] = (pos, path[pos][1]+1, (self.colorMap[ag['name']], dir))
+                            #path[new_pos].append((self.colorMap[ag['name']], dir))
+                            #pdb.set_trace()
+                            self.path = self.backtracePath(path, new_pos)
+                            return path#path[new_pos]
                         queue.append(new_pos)
                         visited.append(new_pos)
-                        path[new_pos] = path[pos].copy()
-                        path[new_pos].append((self.colorMap[ag['name']], dir))
-                        depth = len(path[new_pos])
+                        #path[new_pos] = path[pos].copy()
+                        path[new_pos] = (pos, path[pos][1]+1, (self.colorMap[ag['name']], dir))
+                        #path[new_pos].append((self.colorMap[ag['name']], dir))
+                        depth = path[new_pos][1]
                         if depth > depth_old:
                             print('depth:', depth)
                             print('queue length:', len(queue))
@@ -87,5 +104,5 @@ if __name__ == "__main__":
     solve = Solver(game)
     print(solve.agent0)
     print(solve.goal)
-    print(solve.path)
+    #print(solve.path)
     # for solver.path do move
